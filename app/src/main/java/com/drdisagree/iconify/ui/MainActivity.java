5390e297ad470fd6135046fb6eb27e0b674ab967
Mahmud <crazymahmud08@gmail.com>
2022-09-14 00:44:26 +0600

    Added spinner for module installation

--- app/src/main/java/com/drdisagree/iconify/ui/MainActivity.java
+++ app/src/main/java/com/drdisagree/iconify/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.drdisagree.iconify.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -11,8 +12,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.drdisagree.iconify.BuildConfig;
-import com.drdisagree.iconify.config.PrefConfig;
 import com.drdisagree.iconify.R;
+import com.drdisagree.iconify.config.PrefConfig;
 import com.drdisagree.iconify.utils.OverlayUtils;
 import com.drdisagree.iconify.utils.RootUtil;
 
@@ -22,12 +23,16 @@ public class MainActivity extends AppCompatActivity {
 
     private final int versionCode = BuildConfig.VERSION_CODE;
     private final String versionName = BuildConfig.VERSION_NAME;
+    private LinearLayout spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Progressbar while installing module
+        spinner = findViewById(R.id.progressBar_installingModule);
 
         // Continue button
         Button checkRoot = findViewById(R.id.checkRoot);
@@ -46,11 +51,36 @@ public class MainActivity extends AppCompatActivity {
             if (RootUtil.isDeviceRooted()) {
                 if (RootUtil.isMagiskInstalled()) {
                     if ((PrefConfig.loadPrefInt(this, "versionCode") < versionCode) || !OverlayUtils.moduleExists()) {
-                        try {
-                            OverlayUtils.handleModule(this);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+
+                        // Show spinner
+                        spinner.setVisibility(View.VISIBLE);
+
+                        // Block touch
+                        getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE, WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+
+                        Runnable runnable = new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    OverlayUtils.handleModule(getApplicationContext());
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        };
+                        Thread thread = new Thread(runnable);
+                        thread.start();
+
+                        // Wait 2 second
+                        spinner.postDelayed(new Runnable() {
+                            public void run() {
+                                // Hide spinner
+                                spinner.setVisibility(View.GONE);
+                                // Unblock touch
+                                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                            }
+                        }, 2000);
+
                         if (PrefConfig.loadPrefInt(this, "versionCode") != 0)
                             Toast.makeText(getApplicationContext(), "Reboot to Apply Changes", Toast.LENGTH_LONG).show();
                     }
@@ -60,11 +90,36 @@ public class MainActivity extends AppCompatActivity {
                         startActivity(intent);
                         finish();
                     } else {
-                        try {
-                            OverlayUtils.handleModule(this);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+
+                        // Show spinner
+                        spinner.setVisibility(View.VISIBLE);
+
+                        // Block touch
+                        getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE, WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+
+                        Runnable runnable = new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    OverlayUtils.handleModule(getApplicationContext());
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        };
+                        Thread thread = new Thread(runnable);
+                        thread.start();
+
+                        // Wait 2 second
+                        spinner.postDelayed(new Runnable() {
+                            public void run() {
+                                // Hide spinner
+                                spinner.setVisibility(View.GONE);
+                                // Unblock touch
+                                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                            }
+                        }, 2000);
+
                         warn.setVisibility(View.VISIBLE);
                         warning.setText("Reboot your device first!");
                     }
