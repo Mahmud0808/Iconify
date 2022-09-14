6469ab50f812550d80f4237245c1e1bf4bbf0035
Mahmud <crazymahmud08@gmail.com>
2022-09-14 22:30:51 +0600

    Added fabricated overlay manager

---
+++ app/src/main/java/com/drdisagree/iconify/utils/FabricatedOverlay.java
@@ -0,0 +1,50 @@
+package com.drdisagree.iconify.utils;
+
+import com.drdisagree.iconify.SplashActivity;
+import com.drdisagree.iconify.config.PrefConfig;
+import com.topjohnwu.superuser.Shell;
+
+import java.util.List;
+
+public class FabricatedOverlay {
+
+    public static List<String> getOverlayList() {
+    return Shell.cmd("cmd overlay list |  grep -E '^....com.android.shell:IconifyComponent' | sed -E 's/^....//'").exec().getOut();
+}
+
+    public static List<String> getEnabledOverlayList() {
+        return Shell.cmd("cmd overlay list |  grep -E '^.x..com.android.shell:IconifyComponent' | sed -E 's/^....//'").exec().getOut();
+    }
+
+    public static void buildOverlay(String target, String name, String type, String resourceName, String val) {
+        disableOverlay(name);
+
+        String resourceType = "0x1c";
+
+        if (target.equals("systemui"))
+            target = "com.android.systemui";
+
+        if (type.equals("color"))
+            resourceType = "0x1c";
+        else if (type.equals("dimen"))
+            resourceType = "0x05";
+
+        Shell.cmd("cmd overlay fabricate --target " + target + " --name IconifyComponent" + name + " " + target + ":" + type + "/" + resourceName + " " + resourceType + " " + val).exec();
+    }
+
+    public static void enableOverlay(String name) {
+        Shell.cmd("cmd overlay enable --user current com.android.shell:IconifyComponent" + name, "cmd overlay set-priority com.android.shell:IconifyComponent" + name + " highest").exec();
+    }
+
+    public static void disableOverlay(String name) {
+        Shell.cmd("cmd overlay disable --user current com.android.shell:IconifyComponent" + name).exec();
+    }
+
+    public static boolean isOverlayEnabled(String name) {
+        return PrefConfig.loadPrefBool(SplashActivity.getContext(), "com.android.shell:IconifyComponent" + name);
+    }
+
+    public static boolean isOverlayDisabled(String name) {
+        return !PrefConfig.loadPrefBool(SplashActivity.getContext(), "com.android.shell:IconifyComponent" + name);
+    }
+}
