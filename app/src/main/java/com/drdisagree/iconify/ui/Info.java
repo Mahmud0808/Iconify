668e5f6cf24e133115e0a54f343f428cd44157ca
Mahmud <crazymahmud08@gmail.com>
2022-09-17 01:33:50 +0600

    Added 1perialf and ritesh on credit section

--- app/src/main/java/com/drdisagree/iconify/ui/Info.java
+++ app/src/main/java/com/drdisagree/iconify/ui/Info.java
@@ -76,6 +76,8 @@ public class Info extends AppCompatActivity {
         telegramDesc.setText("Follow to get latest news & updates.");
 
         // Credits
+
+        // Icons8
         ViewGroup creditIcons8 = findViewById(R.id.creditIcons8);
         creditIcons8.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,6 +95,7 @@ public class Info extends AppCompatActivity {
         TextView creditsTo = findViewById(R.id.creditsTo);
         creditsTo.setText("For Plumpy and Fluency icons.");
 
+        // Jai
         ViewGroup jai = findViewById(R.id.jai);
         jai.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -109,6 +112,42 @@ public class Info extends AppCompatActivity {
         jaiName.setText("Jai");
         TextView jaiDesc = findViewById(R.id.jaiDesc);
         jaiDesc.setText("For helping me with Shell Scripts.");
+
+        // 1perialf
+        ViewGroup iperialf = findViewById(R.id.iperialf);
+        iperialf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://t.me/Rodolphe06";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        ImageView img_iperialf = findViewById(R.id.img_iperialf);
+        img_iperialf.setBackgroundResource(R.drawable.ic_user);
+        TextView iperialfName = findViewById(R.id.iperialfName);
+        iperialfName.setText("1perialf");
+        TextView iperialfDesc = findViewById(R.id.iperialfDesc);
+        iperialfDesc.setText("For helping me with RRO.");
+
+        // Ritesh
+        ViewGroup ritesh = findViewById(R.id.ritesh);
+        ritesh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://t.me/ModestCat03";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        ImageView img_ritesh = findViewById(R.id.img_ritesh);
+        img_ritesh.setBackgroundResource(R.drawable.ic_user);
+        TextView riteshName = findViewById(R.id.riteshName);
+        riteshName.setText("Ritesh");
+        TextView riteshDesc = findViewById(R.id.riteshDesc);
+        riteshDesc.setText("For helping me with RRO.");
     }
 
     @Override
