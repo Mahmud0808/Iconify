004de432359cd3a5404f296ab6f1701d3741372b
Mahmud <crazymahmud08@gmail.com>
2022-11-13 13:44:34 +0600

    Initial commit

---
+++ app/src/main/java/com/drdisagree/iconify/installer/RadiusInstaller.java
@@ -0,0 +1,70 @@
+package com.drdisagree.iconify.installer;
+
+import com.drdisagree.iconify.Iconify;
+import com.drdisagree.iconify.config.PrefConfig;
+import com.drdisagree.iconify.services.ApplyOnBoot;
+import com.topjohnwu.superuser.Shell;
+
+import java.io.File;
+
+public class RadiusInstaller {
+
+    private static final int TOTAL_RADIUS = 58;
+
+    public static void install_pack(int n) {
+        disable_others(n);
+        enable_pack(n);
+    }
+
+    protected static void enable_pack(int n) {
+
+        String path = "/system/product/overlay/IconifyComponentCR" + n + ".apk";
+
+        if (new File(path).exists()) {
+
+            String overlay = (path.replaceAll("/system/product/overlay/", "")).replaceAll("apk", "overlay");
+
+            try {
+                Shell.cmd("cmd overlay enable --user current " + overlay).exec();
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+        }
+    }
+
+    public static void disable_pack(int n) {
+
+        String path = "/system/product/overlay/IconifyComponentCR" + n + ".apk";
+
+        if (new File(path).exists()) {
+
+            String overlay = (path.replaceAll("/system/product/overlay/", "")).replaceAll("apk", "overlay");
+
+            try {
+                Shell.cmd("cmd overlay disable --user current " + overlay).exec();
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+        }
+    }
+
+    protected static void disable_others(int n) {
+
+        for (int i = 0; i <= TOTAL_RADIUS; i++) {
+            if (i != n) {
+                String path = "/system/product/overlay/IconifyComponentCR" + i + ".apk";
+
+                if (new File(path).exists()) {
+
+                    String overlay = (path.replaceAll("/system/product/overlay/", "")).replaceAll("apk", "overlay");
+
+                    try {
+                        Shell.cmd("cmd overlay disable --user current " + overlay).exec();
+                    } catch (Throwable t) {
+                        t.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
