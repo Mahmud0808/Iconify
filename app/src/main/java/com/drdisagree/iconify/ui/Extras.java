a67bb74266309dba3bdb7048d584bd96b9c0433a
Mahmud <crazymahmud08@gmail.com>
2022-11-13 18:52:50 +0600

    Added live preview with confirmation button for UI Radius

--- app/src/main/java/com/drdisagree/iconify/ui/Extras.java
+++ app/src/main/java/com/drdisagree/iconify/ui/Extras.java
@@ -1,6 +1,8 @@
 package com.drdisagree.iconify.ui;
 
 import android.annotation.SuppressLint;
+import android.content.res.Configuration;
+import android.graphics.drawable.GradientDrawable;
 import android.graphics.text.LineBreaker;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,7 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -71,53 +74,87 @@ public class Extras extends AppCompatActivity {
 
         // Corner Radius
 
+        LinearLayout qstile_preview1 = findViewById(R.id.qs_tile_preview1);
+        LinearLayout qstile_preview2 = findViewById(R.id.qs_tile_preview2);
+        LinearLayout qstile_preview3 = findViewById(R.id.qs_tile_preview3);
+        LinearLayout qstile_preview4 = findViewById(R.id.qs_tile_preview4);
+
+        GradientDrawable drawable1 = (GradientDrawable) qstile_preview1.getBackground();
+        GradientDrawable drawable2 = (GradientDrawable) qstile_preview2.getBackground();
+        GradientDrawable drawable3 = (GradientDrawable) qstile_preview3.getBackground();
+        GradientDrawable drawable4 = (GradientDrawable) qstile_preview4.getBackground();
+
         SeekBar corner_radius_seekbar = findViewById(R.id.corner_radius_seekbar);
         TextView corner_radius_output = findViewById(R.id.corner_radius_output);
 
         corner_radius_seekbar.setPadding(0, 0, 0, 0);
         final int[] finalUiCornerRadius = {16};
+        if (!PrefConfig.loadPrefSettings(Iconify.getAppContext(), "cornerRadius").equals("null"))
+            finalUiCornerRadius[0] = Integer.parseInt(PrefConfig.loadPrefSettings(Iconify.getAppContext(), "cornerRadius"));
 
         if (!PrefConfig.loadPrefSettings(Iconify.getAppContext(), "cornerRadius").equals("null")) {
-            if ((Integer.parseInt(PrefConfig.loadPrefSettings(Iconify.getAppContext(), "cornerRadius")) + 8) == 24)
+            if ((Integer.parseInt(PrefConfig.loadPrefSettings(Iconify.getAppContext(), "cornerRadius")) + 8) == 24) {
                 corner_radius_output.setText("Selected: " + (Integer.parseInt(PrefConfig.loadPrefSettings(Iconify.getAppContext(), "cornerRadius")) + 8) + "dp (Default)");
-            else
+            } else {
                 corner_radius_output.setText("Selected: " + (Integer.parseInt(PrefConfig.loadPrefSettings(Iconify.getAppContext(), "cornerRadius")) + 8) + "dp");
+            }
+            drawable1.setCornerRadius((Integer.parseInt(PrefConfig.loadPrefSettings(Iconify.getAppContext(), "cornerRadius"))  + 8) * getResources().getDisplayMetrics().density);
+            drawable2.setCornerRadius((Integer.parseInt(PrefConfig.loadPrefSettings(Iconify.getAppContext(), "cornerRadius"))  + 8) * getResources().getDisplayMetrics().density);
+            drawable3.setCornerRadius((Integer.parseInt(PrefConfig.loadPrefSettings(Iconify.getAppContext(), "cornerRadius"))  + 8) * getResources().getDisplayMetrics().density);
+            drawable4.setCornerRadius((Integer.parseInt(PrefConfig.loadPrefSettings(Iconify.getAppContext(), "cornerRadius"))  + 8) * getResources().getDisplayMetrics().density);
             finalUiCornerRadius[0] = Integer.parseInt(PrefConfig.loadPrefSettings(Iconify.getAppContext(), "cornerRadius"));
             corner_radius_seekbar.setProgress(finalUiCornerRadius[0]);
+        } else {
+            drawable1.setCornerRadius(24 * getResources().getDisplayMetrics().density);
+            drawable2.setCornerRadius(24 * getResources().getDisplayMetrics().density);
+            drawable3.setCornerRadius(24 * getResources().getDisplayMetrics().density);
+            drawable4.setCornerRadius(24 * getResources().getDisplayMetrics().density);
         }
 
         corner_radius_seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-                PrefConfig.savePrefBool(Iconify.getAppContext(), "fabricatedcornerRadius", true);
+                ;
             }
 
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
                 finalUiCornerRadius[0] = progress;
                 if (progress + 8 == 24)
                     corner_radius_output.setText("Selected: " + (progress + 8) + "dp (Default)");
                 else
                     corner_radius_output.setText("Selected: " + (progress + 8) + "dp");
-
+                drawable1.setCornerRadius((finalUiCornerRadius[0] + 8) * getResources().getDisplayMetrics().density);
+                drawable2.setCornerRadius((finalUiCornerRadius[0] + 8) * getResources().getDisplayMetrics().density);
+                drawable3.setCornerRadius((finalUiCornerRadius[0] + 8) * getResources().getDisplayMetrics().density);
+                drawable4.setCornerRadius((finalUiCornerRadius[0] + 8) * getResources().getDisplayMetrics().density);
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
+                ;
+            }
+        });
 
-                Runnable runnable = new Runnable() {
-                    @Override
+        Button apply_radius = findViewById(R.id.apply_radius);
+        apply_radius.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show spinner
+                spinner.setVisibility(View.VISIBLE);
+                // Block touch
+                getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE, WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                spinner.postDelayed(new Runnable() {
                     public void run() {
                         RadiusInstaller.install_pack(finalUiCornerRadius[0]);
                         PrefConfig.savePrefSettings(Iconify.getAppContext(), "cornerRadius", String.valueOf(finalUiCornerRadius[0]));
+                        // Hide spinner
+                        spinner.setVisibility(View.GONE);
+                        // Unblock touch
+                        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
                     }
-                };
-                Thread thread = new Thread(runnable);
-                thread.start();
-
-                Toast.makeText(getApplicationContext(), (finalUiCornerRadius[0] + 8 + "dp Applied"), Toast.LENGTH_SHORT).show();
+                }, 1000);
             }
         });
 
@@ -177,11 +214,30 @@ public class Extras extends AppCompatActivity {
                 }, 1000);
             }
         });
+
+        // Change orientation in landscape / portrait mode
+        int orientation = this.getResources().getConfiguration().orientation;
+        LinearLayout qs_tile_preview_orientation = findViewById(R.id.qs_tile_preview_orientation);
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE)
+            qs_tile_preview_orientation.setOrientation(LinearLayout.HORIZONTAL);
+        else
+            qs_tile_preview_orientation.setOrientation(LinearLayout.VERTICAL);
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    // Change orientation in landscape / portrait mode
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        LinearLayout qs_tile_preview_orientation = findViewById(R.id.qs_tile_preview_orientation);
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE)
+            qs_tile_preview_orientation.setOrientation(LinearLayout.HORIZONTAL);
+        else
+            qs_tile_preview_orientation.setOrientation(LinearLayout.VERTICAL);
     }
 }
