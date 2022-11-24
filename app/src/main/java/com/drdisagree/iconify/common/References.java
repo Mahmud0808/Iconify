0a6ee9d0f82444cd5392901c0e20275d002339c6
DrDisagree <crazymahmud08@gmail.com>
2022-11-24 13:50:52 +0600

    Grab variable values dynamically

--- app/src/main/java/com/drdisagree/iconify/common/References.java
+++ app/src/main/java/com/drdisagree/iconify/common/References.java
@@ -10,4 +10,7 @@ public class References {
     public static final int TOTAL_QSSHAPES = Shell.cmd("cmd overlay list |  grep -E '^....IconifyComponentQSSN' | sed -E 's/^....//'").exec().getOut().size();
     public static final int TOTAL_QSSHAPESPIXEL = Shell.cmd("cmd overlay list |  grep -E '^....IconifyComponentQSSP' | sed -E 's/^....//'").exec().getOut().size();
     public static final int TOTAL_RADIUS = Shell.cmd("cmd overlay list |  grep -E '^....IconifyComponentCR' | sed -E 's/^....//'").exec().getOut().size();
+    public static final int TOTAL_TEXTSIZE = Shell.cmd("cmd overlay list |  grep -E '^....IconifyComponentTextSize' | sed -E 's/^....//'").exec().getOut().size();
+    public static final int TOTAL_ICONSIZE = Shell.cmd("cmd overlay list |  grep -E '^....IconifyComponentIconSize' | sed -E 's/^....//'").exec().getOut().size();
+    public static final int TOTAL_MOVEICON = Shell.cmd("cmd overlay list |  grep -E '^....IconifyComponentMoveIcon' | sed -E 's/^....//'").exec().getOut().size();
 }
