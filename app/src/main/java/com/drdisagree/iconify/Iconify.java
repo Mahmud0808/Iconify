4716f3c19a361dec6c0a73d34790c2845caf9b0c
DrDisagree <crazymahmud08@gmail.com>
2023-01-16 22:54:12 +0600

    Remove unused context for preferences

--- app/src/main/java/com/drdisagree/iconify/Iconify.java
+++ app/src/main/java/com/drdisagree/iconify/Iconify.java
@@ -3,6 +3,7 @@ package com.drdisagree.iconify;
 import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.Context;
+import android.os.StrictMode;
 
 import com.google.android.material.color.DynamicColors;
 
@@ -10,20 +11,14 @@ public class Iconify extends Application {
 
     @SuppressLint("StaticFieldLeak")
     private static Context context;
-    private static Context prefContext;
 
     public static Context getAppContext() {
         return Iconify.context;
     }
 
-    public static Context getPrefContext() {
-        return Iconify.prefContext;
-    }
-
     public void onCreate() {
         super.onCreate();
         Iconify.context = getApplicationContext();
-        Iconify.prefContext = getApplicationContext().createDeviceProtectedStorageContext();
         DynamicColors.applyToActivitiesIfAvailable(this);
     }
 }
