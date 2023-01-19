4e4dbef2a9f1eb1374cabf160ba96e8aec4399ab
DrDisagree <crazymahmud08@gmail.com>
2023-01-19 21:45:51 +0600

    Add credit and license boilerplate

--- app/src/main/java/com/drdisagree/iconify/xposed/mods/VerticalQSTile.java
+++ app/src/main/java/com/drdisagree/iconify/xposed/mods/VerticalQSTile.java
@@ -1,5 +1,22 @@
 package com.drdisagree.iconify.xposed.mods;
 
+/* Modified from AOSPMods
+ * https://github.com/siavash79/AOSPMods/blob/canary/app/src/main/java/sh/siava/AOSPMods/systemui/QSTileGrid.java
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/].
+ */
+
 import static com.drdisagree.iconify.common.References.HIDE_QSLABEL_SWITCH;
 import static com.drdisagree.iconify.common.References.SYSTEM_UI_PACKAGE;
 import static com.drdisagree.iconify.common.References.VERTICAL_QSTILE_SWITCH;
