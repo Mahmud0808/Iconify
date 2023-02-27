0d209bfc370b3dcff28b54764f8cdd96f232870f
Nady <mohamed.amr.nes@gmail.com>
2023-02-27 11:46:19 +0200

    add ksu check

--- app/src/main/java/com/drdisagree/iconify/utils/RootUtil.java
+++ app/src/main/java/com/drdisagree/iconify/utils/RootUtil.java
@@ -14,6 +14,10 @@ public class RootUtil {
         return Shell.cmd("[ -d /data/adb/magisk ]").exec().isSuccess();
     }
 
+    public static boolean isKSUInstalled() {
+        return Shell.cmd("[ -d /data/adb/ksu ]").exec().isSuccess();
+    }
+
     public static void setPermissions(final int permission, final String filename) {
         Shell.cmd("chmod " + permission + ' ' + filename).exec();
     }
@@ -23,14 +27,17 @@ public class RootUtil {
 
         String perm = String.valueOf(permission);
 
-        if (!Shell.cmd("stat -c '%a' " + foldername).exec().getOut().contains(perm) || !Shell.cmd("fl=$(find '" + foldername + "' -type f -mindepth 1 -print -quit); stat -c '%a' $fl").exec().getOut().contains(perm))
+        if (!Shell.cmd("stat -c '%a' " + foldername).exec().getOut().contains(perm)
+                || !Shell.cmd("fl=$(find '" + foldername + "' -type f -mindepth 1 -print -quit); stat -c '%a' $fl")
+                        .exec().getOut().contains(perm))
             Shell.cmd("for file in " + foldername + "*; do chmod " + permission + " \"$file\"; done").exec();
     }
 
     public static boolean fileExists(String dir) {
         List<String> lines = Shell.cmd("test -f " + dir + " && echo '1'").exec().getOut();
         for (String line : lines) {
-            if (line.contains("1")) return true;
+            if (line.contains("1"))
+                return true;
         }
         return false;
     }
@@ -38,7 +45,8 @@ public class RootUtil {
     public static boolean folderExists(String dir) {
         List<String> lines = Shell.cmd("test -d " + dir + " && echo '1'").exec().getOut();
         for (String line : lines) {
-            if (line.contains("1")) return true;
+            if (line.contains("1"))
+                return true;
         }
         return false;
     }
