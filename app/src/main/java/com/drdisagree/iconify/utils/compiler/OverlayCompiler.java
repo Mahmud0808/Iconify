3cb3dee1b5ae97015f1343444bbc616ff80e8fc5
DrDisagree <crazymahmud08@gmail.com>
2023-03-04 02:58:21 +0600

    Module Installer: Retry 3 times before throwing an error

--- app/src/main/java/com/drdisagree/iconify/utils/compiler/OverlayCompiler.java
+++ app/src/main/java/com/drdisagree/iconify/utils/compiler/OverlayCompiler.java
@@ -10,20 +10,14 @@ import android.util.Log;
 
 import com.drdisagree.iconify.Iconify;
 import com.drdisagree.iconify.common.Resources;
-import com.drdisagree.iconify.utils.FileUtil;
-import com.drdisagree.iconify.utils.RootUtil;
 import com.drdisagree.iconify.utils.apksigner.JarMap;
 import com.drdisagree.iconify.utils.apksigner.SignAPK;
-import com.drdisagree.iconify.utils.helpers.BackupRestore;
 import com.topjohnwu.superuser.Shell;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
-import java.util.Objects;
 
 public class OverlayCompiler {
 
@@ -32,38 +26,76 @@ public class OverlayCompiler {
     private static final String zipalign = ZIPALIGN.getAbsolutePath();
 
     public static boolean createManifest(String name, String target, String source) {
-        Shell.Result result = Shell.cmd("printf '<?xml version=\"1.0\" encoding=\"utf-8\" ?>\\n<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\" android:versionName=\"v1.0\" package=\"IconifyComponent" + name + ".overlay\">\\n\\t<overlay android:priority=\"1\" android:targetPackage=\"" + target + "\" />\\n\\t<application android:allowBackup=\"false\" android:hasCode=\"false\" />\\n</manifest>' > " + source + "/AndroidManifest.xml;").exec();
+        Shell.Result result = null;
+        int attempt = 3;
 
-        if (result.isSuccess())
-            Log.i(TAG + " - Manifest", "Successfully created manifest for " + name);
-        else {
-            Log.e(TAG + " - Manifest", "Failed to create manifest for " + name + '\n' + String.join("\n", result.getOut()));
-            writeLog(TAG + " - Manifest", "Failed to create manifest for " + name, result.getOut());
+        while (attempt-- != 0) {
+            result = Shell.cmd("printf '<?xml version=\"1.0\" encoding=\"utf-8\" ?>\\n<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\" android:versionName=\"v1.0\" package=\"IconifyComponent" + name + ".overlay\">\\n\\t<overlay android:priority=\"1\" android:targetPackage=\"" + target + "\" />\\n\\t<application android:allowBackup=\"false\" android:hasCode=\"false\" />\\n</manifest>' > " + source + "/AndroidManifest.xml;").exec();
+
+            if (result.isSuccess()) {
+                Log.i(TAG + " - Manifest", "Successfully created manifest for " + name);
+                break;
+            } else {
+                Log.e(TAG + " - Manifest", "Failed to create manifest for " + name + '\n' + String.join("\n", result.getOut()));
+                try {
+                    Thread.sleep(2000);
+                } catch (Exception ignored) {
+                }
+            }
         }
+
+        if (!result.isSuccess())
+            writeLog(TAG + " - Manifest", "Failed to create manifest for " + name, result.getOut());
 
         return !result.isSuccess();
     }
 
     public static boolean runAapt(String source, String name) {
-        Shell.Result result = Shell.cmd(aapt + " p -f -v -M " + source + "/AndroidManifest.xml -I /system/framework/framework-res.apk -S " + source + "/res -F " + Resources.UNSIGNED_UNALIGNED_DIR + '/' + name + "-unsigned-unaligned.apk >/dev/null;").exec();
+        Shell.Result result = null;
+        int attempt = 3;
 
-        if (result.isSuccess()) Log.i(TAG + " - AAPT", "Successfully built APK for " + name);
-        else {
-            Log.e(TAG + " - AAPT", "Failed to build APK for " + name + '\n' + String.join("\n", result.getOut()));
-            writeLog(TAG + " - AAPT", "Failed to build APK for " + name, result.getOut());
+        while (attempt-- != 0) {
+            result = Shell.cmd(aapt + " p -f -M " + source + "/AndroidManifest.xml -I /system/framework/framework-res.apk -S " + source + "/res -F " + Resources.UNSIGNED_UNALIGNED_DIR + '/' + name + "-unsigned-unaligned.apk >/dev/null;").exec();
+
+            if (result.isSuccess()) {
+                Log.i(TAG + " - AAPT", "Successfully built APK for " + name);
+                break;
+            } else {
+                Log.e(TAG + " - AAPT", "Failed to build APK for " + name + '\n' + String.join("\n", result.getOut()));
+                try {
+                    Thread.sleep(2000);
+                } catch (Exception ignored) {
+                }
+            }
         }
+
+        if (!result.isSuccess())
+            writeLog(TAG + " - AAPT", "Failed to build APK for " + name, result.getOut());
 
         return !result.isSuccess();
     }
 
     public static boolean zipAlign(String source, String name) {
-        Shell.Result result = Shell.cmd(zipalign + " 4 " + source + ' ' + Resources.UNSIGNED_DIR + '/' + name).exec();
+        Shell.Result result = null;
+        int attempt = 3;
 
-        if (result.isSuccess()) Log.i(TAG + " - ZipAlign", "Successfully zip aligned " + name);
-        else {
-            Log.e(TAG + " - ZipAlign", "Failed to zip align " + name + '\n' + String.join("\n", result.getOut()));
-            writeLog(TAG + " - ZipAlign", "Failed to zip align " + name, result.getOut());
+        while (attempt-- != 0) {
+            result = Shell.cmd(zipalign + " -p -f 4 " + source + ' ' + Resources.UNSIGNED_DIR + '/' + name).exec();
+
+            if (result.isSuccess()) {
+                Log.i(TAG + " - ZipAlign", "Successfully zip aligned " + name);
+                break;
+            } else {
+                Log.e(TAG + " - ZipAlign", "Failed to zip align " + name + '\n' + String.join("\n", result.getOut()));
+                try {
+                    Thread.sleep(2000);
+                } catch (Exception ignored) {
+                }
+            }
         }
+
+        if (!result.isSuccess())
+            writeLog(TAG + " - ZipAlign", "Failed to zip align " + name, result.getOut());
 
         return !result.isSuccess();
     }
