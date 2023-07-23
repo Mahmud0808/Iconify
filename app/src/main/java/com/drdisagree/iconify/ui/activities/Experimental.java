35b466a2d80c880e2fbd400348fd73dee68f3d8f
DrDisagree <crazymahmud08@gmail.com>
2023-07-23 17:44:05 +0600

    Experimental: Fix overlap header image save state

--- app/src/main/java/com/drdisagree/iconify/ui/activities/Experimental.java
+++ app/src/main/java/com/drdisagree/iconify/ui/activities/Experimental.java
@@ -1,14 +1,12 @@
 package com.drdisagree.iconify.ui.activities;
 
 import static com.drdisagree.iconify.common.Const.SWITCH_ANIMATION_DELAY;
-import static com.drdisagree.iconify.common.Preferences.HEADER_CLOCK_SWITCH;
 import static com.drdisagree.iconify.common.Preferences.HEADER_IMAGE_OVERLAP;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.widget.CompoundButton;
 
 import com.drdisagree.iconify.R;
 import com.drdisagree.iconify.config.RPrefs;
@@ -30,6 +28,7 @@ public class Experimental extends BaseActivity {
         // Header
         ViewBindingHelpers.setHeader(this, binding.header.collapsingToolbar, binding.header.toolbar, R.string.activity_title_experimental);
 
+        binding.headerImageOverlap.setChecked(RPrefs.getBoolean(HEADER_IMAGE_OVERLAP, false));
         binding.headerImageOverlap.setOnCheckedChangeListener((compoundButton, isChecked) -> {
             RPrefs.putBoolean(HEADER_IMAGE_OVERLAP, isChecked);
             new Handler(Looper.getMainLooper()).postDelayed(SystemUtil::restartSystemUI, SWITCH_ANIMATION_DELAY);
