374bcf9bbe36e457a9ecfa8a89a1d491ee5ce50e
DrDisagree <crazymahmud08@gmail.com>
2023-07-23 23:59:04 +0600

    Xposed: Move "Hide Data Disabled Icon" to Experimental

--- app/src/main/java/com/drdisagree/iconify/ui/activities/Experimental.java
+++ app/src/main/java/com/drdisagree/iconify/ui/activities/Experimental.java
@@ -2,6 +2,7 @@ package com.drdisagree.iconify.ui.activities;
 
 import static com.drdisagree.iconify.common.Const.SWITCH_ANIMATION_DELAY;
 import static com.drdisagree.iconify.common.Preferences.HEADER_IMAGE_OVERLAP;
+import static com.drdisagree.iconify.common.Preferences.HIDE_DATA_DISABLED_ICON;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
@@ -28,10 +29,17 @@ public class Experimental extends BaseActivity {
         // Header
         ViewBindingHelpers.setHeader(this, binding.header.collapsingToolbar, binding.header.toolbar, R.string.activity_title_experimental);
 
+        // Header image overlap
         binding.headerImageOverlap.setChecked(RPrefs.getBoolean(HEADER_IMAGE_OVERLAP, false));
         binding.headerImageOverlap.setOnCheckedChangeListener((compoundButton, isChecked) -> {
             RPrefs.putBoolean(HEADER_IMAGE_OVERLAP, isChecked);
             new Handler(Looper.getMainLooper()).postDelayed(SystemUtil::restartSystemUI, SWITCH_ANIMATION_DELAY);
+        });
+
+        // Hide data disabled icon
+        binding.hideDataDisabledIcon.setChecked(RPrefs.getBoolean(HIDE_DATA_DISABLED_ICON, false));
+        binding.hideDataDisabledIcon.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            RPrefs.putBoolean(HIDE_DATA_DISABLED_ICON, isChecked);
         });
     }
 }
