047da1a118159a958f4893dd903d1caf8c610a04
DrDisagree <crazymahmud08@gmail.com>
2023-08-19 21:27:17 +0600

    Refactor and add Spanish translation credit

--- app/src/main/java/com/drdisagree/iconify/ui/activities/Info.java
+++ app/src/main/java/com/drdisagree/iconify/ui/activities/Info.java
@@ -108,6 +108,7 @@ public class Info extends BaseActivity {
         translators_list.add(new InfoModel(this, "ElTifo", getResources().getString(R.string.pt_translation), "https://github.com/ElTifo", R.drawable.ic_user));
         translators_list.add(new InfoModel(this, "Blays", getResources().getString(R.string.ru_translation), "https://github.com/B1ays", R.drawable.ic_user));
         translators_list.add(new InfoModel(this, "Cccc_", getResources().getString(R.string.zh_cn_translation), "https://github.com/Cccc-owo", R.drawable.ic_user));
+        translators_list.add(new InfoModel(this, "luckkmaxx", getResources().getString(R.string.es_translation), "https://github.com/luckkmaxx", R.drawable.ic_user));
         translators_list.add(new InfoModel(this, "Serhat Demir", getResources().getString(R.string.tr_translation), "https://github.com/serhat-demir", R.drawable.ic_user));
         translators_list.add(new InfoModel(this, "Đức Trọng", getResources().getString(R.string.vi_translation), "https://t.me/viettel1211", R.drawable.ic_user));
 
