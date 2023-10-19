410eec48df2a87e1baef2a68f5ad9848d5d1c526
DrDisagree <crazymahmud08@gmail.com>
2023-10-19 19:59:47 +0600

    Fix edge to edge in landscape mode

--- app/src/main/java/com/drdisagree/iconify/ui/base/BaseActivity.java
+++ app/src/main/java/com/drdisagree/iconify/ui/base/BaseActivity.java
@@ -1,13 +1,18 @@
 package com.drdisagree.iconify.ui.base;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.view.Window;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.drdisagree.iconify.R;
 import com.drdisagree.iconify.ui.utils.ThemeHelper;
@@ -38,8 +43,28 @@ public class BaseActivity extends AppCompatActivity {
 
         Window window = getWindow();
         WindowCompat.setDecorFitsSystemWindows(window, false);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            ViewGroup viewGroup = getWindow().getDecorView().findViewById(android.R.id.content);
+            ViewCompat.setOnApplyWindowInsetsListener(viewGroup, (v, windowInsets) -> {
+                Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+
+                ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+                v.setPadding(
+                        params.leftMargin + insets.left,
+                        0,
+                        params.rightMargin + insets.right,
+                        params.bottomMargin + insets.bottom
+                );
+                params.topMargin = 0;
+                v.setLayoutParams(params);
+
+                return windowInsets;
+            });
+        }
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
