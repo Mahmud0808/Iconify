fea83aedc2dfd85effd10c8a75f3e7d14f02e628
DrDisagree <crazymahmud08@gmail.com>
2023-10-19 20:25:06 +0600

    Wrap context with locale resources

--- app/src/main/java/com/drdisagree/iconify/Iconify.java
+++ app/src/main/java/com/drdisagree/iconify/Iconify.java
@@ -3,6 +3,7 @@ package com.drdisagree.iconify;
 import android.app.Application;
 import android.content.Context;
 
+import com.drdisagree.iconify.utils.helper.LocaleHelper;
 import com.google.android.material.color.DynamicColors;
 
 import java.lang.ref.WeakReference;
@@ -15,13 +16,13 @@ public class Iconify extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
-        contextReference = new WeakReference<>(getApplicationContext());
+        contextReference = new WeakReference<>(LocaleHelper.setLocale(getApplicationContext()));
         DynamicColors.applyToActivitiesIfAvailable(this);
     }
 
     public static Context getAppContext() {
         if (contextReference == null || contextReference.get() == null) {
-            contextReference = new WeakReference<>(Iconify.getInstance().getApplicationContext());
+            contextReference = new WeakReference<>(LocaleHelper.setLocale(Iconify.getInstance().getApplicationContext()));
         }
 
         return contextReference.get();
--- app/src/main/java/com/drdisagree/iconify/utils/helper/LocaleHelper.java
+++ app/src/main/java/com/drdisagree/iconify/utils/helper/LocaleHelper.java
@@ -1,19 +1,20 @@
 package com.drdisagree.iconify.utils.helper;
 
 import static com.drdisagree.iconify.common.Preferences.APP_LANGUAGE;
+import static com.drdisagree.iconify.common.Resources.SharedPref;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.LocaleList;
-
-import com.drdisagree.iconify.config.Prefs;
 
 import java.util.Locale;
 
 public class LocaleHelper {
 
     public static Context setLocale(Context context) {
-        String localeCode = Prefs.getString(APP_LANGUAGE, "");
+        SharedPreferences prefs = context.getSharedPreferences(SharedPref, Context.MODE_PRIVATE);
+        String localeCode = prefs.getString(APP_LANGUAGE, "");
 
         if (!localeCode.isEmpty()) {
             Locale locale = Locale.forLanguageTag(localeCode);
