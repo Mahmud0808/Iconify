f82a1e9b46794fd630d4d844056cd444ad4c6fad
DrDisagree <crazymahmud08@gmail.com>
2023-11-01 20:47:22 +0600

    Change "Restart SystemUI" tile icon based on icon pack (Closes #326)

--- app/src/main/java/com/drdisagree/iconify/services/TileRestartSystemUI.java
+++ app/src/main/java/com/drdisagree/iconify/services/TileRestartSystemUI.java
@@ -1,10 +1,12 @@
 package com.drdisagree.iconify.services;
 
+import android.graphics.drawable.Icon;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 
 import com.drdisagree.iconify.R;
 import com.drdisagree.iconify.utils.SystemUtil;
+import com.drdisagree.iconify.utils.overlay.OverlayUtil;
 
 public class TileRestartSystemUI extends TileService {
 
@@ -19,6 +21,8 @@ public class TileRestartSystemUI extends TileService {
         Tile pitchBlackTile = getQsTile();
         pitchBlackTile.setState(Tile.STATE_INACTIVE);
         pitchBlackTile.updateTile();
+
+        updateTileIcon(pitchBlackTile);
     }
 
     @Override
@@ -37,10 +41,28 @@ public class TileRestartSystemUI extends TileService {
         pitchBlackTile.setLabel(getResources().getString(R.string.restart_sysui_title));
         pitchBlackTile.setSubtitle("");
         pitchBlackTile.updateTile();
+
+        updateTileIcon(pitchBlackTile);
     }
 
     @Override
     public void onTileRemoved() {
         super.onTileRemoved();
+    }
+
+    private void updateTileIcon(Tile tile) {
+        int iconResId = R.drawable.ic_tile_restart_systemui;
+
+        if (OverlayUtil.isOverlayEnabled("IconifyComponentIPAS1.overlay")) {
+            iconResId = R.drawable.ic_tile_restart_systemui_aurora;
+        } else if (OverlayUtil.isOverlayEnabled("IconifyComponentIPAS2.overlay")) {
+            iconResId = R.drawable.ic_tile_restart_systemui_gradicon;
+        } else if (OverlayUtil.isOverlayEnabled("IconifyComponentIPAS3.overlay")) {
+            iconResId = R.drawable.ic_tile_restart_systemui_lorn;
+        } else if (OverlayUtil.isOverlayEnabled("IconifyComponentIPAS4.overlay")) {
+            iconResId = R.drawable.ic_tile_restart_systemui_plumpy;
+        }
+
+        tile.setIcon(Icon.createWithResource(this, iconResId));
     }
 }
