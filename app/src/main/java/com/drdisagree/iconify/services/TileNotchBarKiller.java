16409523efd21b44be1c9d7d114db697b6b61182
DrDisagree <crazymahmud08@gmail.com>
2023-11-13 16:36:30 +0600

    Fixed notchbar killer tile

--- app/src/main/java/com/drdisagree/iconify/services/TileNotchBarKiller.java
+++ app/src/main/java/com/drdisagree/iconify/services/TileNotchBarKiller.java
@@ -1,15 +1,20 @@
 package com.drdisagree.iconify.services;
 
+import static com.drdisagree.iconify.common.Const.FRAMEWORK_PACKAGE;
+import static com.drdisagree.iconify.common.Preferences.NOTCH_BAR_KILLER_SWITCH;
+
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 
 import com.drdisagree.iconify.R;
 import com.drdisagree.iconify.config.Prefs;
-import com.drdisagree.iconify.utils.overlay.OverlayUtil;
+import com.drdisagree.iconify.utils.SystemUtil;
+import com.drdisagree.iconify.utils.overlay.manager.resource.ResourceEntry;
+import com.drdisagree.iconify.utils.overlay.manager.resource.ResourceManager;
 
 public class TileNotchBarKiller extends TileService {
 
-    private boolean isNotchBarKillerEnabled = Prefs.getBoolean("IconifyComponentNBK.overlay");
+    private boolean isNotchBarKillerEnabled = Prefs.getBoolean(NOTCH_BAR_KILLER_SWITCH, false);
 
     @Override
     public void onTileAdded() {
@@ -31,21 +36,40 @@ public class TileNotchBarKiller extends TileService {
 
     @Override
     public void onClick() {
-        super.onClick();
-
-        if (isNotchBarKillerEnabled) {
-            OverlayUtil.disableOverlay("IconifyComponentNBK.overlay");
-        } else {
-            OverlayUtil.enableOverlay("IconifyComponentNBK.overlay");
+        if (!SystemUtil.hasStoragePermission()) {
+            Tile tile = getQsTile();
+            tile.setSubtitle(getResources().getString(R.string.need_storage_perm_title));
+            tile.updateTile();
+            return;
         }
 
+        super.onClick();
+
         isNotchBarKillerEnabled = !isNotchBarKillerEnabled;
+
+        Prefs.putBoolean(NOTCH_BAR_KILLER_SWITCH, isNotchBarKillerEnabled);
 
         Tile tile = getQsTile();
         tile.setState(isNotchBarKillerEnabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
         tile.setLabel(getResources().getString(R.string.notch_bar_killer_title));
         tile.setSubtitle(isNotchBarKillerEnabled ? getResources().getString(R.string.general_on) : getResources().getString(R.string.general_off));
         tile.updateTile();
+
+        if (isNotchBarKillerEnabled) {
+            ResourceManager.buildOverlayWithResource(
+                    getApplicationContext(),
+                    new ResourceEntry(FRAMEWORK_PACKAGE, "bool", "config_fillMainBuiltInDisplayCutout", "true"),
+                    new ResourceEntry(FRAMEWORK_PACKAGE, "string", "config_mainBuiltInDisplayCutout", "M 0,0 L 0, 0 C 0,0 0,0 0,0"),
+                    new ResourceEntry(FRAMEWORK_PACKAGE, "string", "config_mainBuiltInDisplayCutoutRectApproximation", "@string/config_mainBuiltInDisplayCutout")
+            );
+        } else {
+            ResourceManager.removeResourceFromOverlay(
+                    getApplicationContext(),
+                    new ResourceEntry(FRAMEWORK_PACKAGE, "bool", "config_fillMainBuiltInDisplayCutout"),
+                    new ResourceEntry(FRAMEWORK_PACKAGE, "string", "config_mainBuiltInDisplayCutout"),
+                    new ResourceEntry(FRAMEWORK_PACKAGE, "string", "config_mainBuiltInDisplayCutoutRectApproximation")
+            );
+        }
     }
 
     @Override
