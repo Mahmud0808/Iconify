381134ef8784798cb7f75fafa6381b5ef06604e7
Saurav Sonkar <122232433+sauravultra@users.noreply.github.com>
2025-03-23 20:55:15 +0530

    Update BatteryStyleManager.kt removed m

--- app/src/main/java/com/drdisagree/iconify/xposed/modules/BatteryStyleManager.kt
+++ app/src/main/java/com/drdisagree/iconify/xposed/modules/BatteryStyleManager.kt
@@ -181,7 +181,7 @@ class BatteryStyleManager(context: Context) : ModPack(context) {
             val hidePercentage: Boolean = getBoolean(CUSTOM_BATTERY_HIDE_PERCENTAGE, false)
             val defaultInsidePercentage = batteryStyle == BATTERY_STYLE_LANDSCAPE_IOS_16 ||
                     batteryStyle == BATTERY_STYLE_LANDSCAPE_BATTERYL ||
-                    batteryStyle == BATTERY_STYLE_LANDSCAPE_BATTERYM
+                    
             val insidePercentage = defaultInsidePercentage ||
                     getBoolean(CUSTOM_BATTERY_INSIDE_PERCENTAGE, false)
             defaultLandscapeBatteryEnabled = batteryStyle == BATTERY_STYLE_DEFAULT_LANDSCAPE ||
