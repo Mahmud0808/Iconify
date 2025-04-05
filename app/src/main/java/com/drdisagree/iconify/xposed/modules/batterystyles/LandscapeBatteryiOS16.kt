494954e8439316791703ce34b52d979a3b3b1a1b
Saurav Sonkar <122232433+sauravultra@users.noreply.github.com>
2025-04-05 17:12:48 +0530

    Update LandscapeBatteryiOS16.kt

--- app/src/main/java/com/drdisagree/iconify/xposed/modules/batterystyles/LandscapeBatteryiOS16.kt
+++ app/src/main/java/com/drdisagree/iconify/xposed/modules/batterystyles/LandscapeBatteryiOS16.kt
@@ -37,7 +37,7 @@ import com.drdisagree.iconify.R
 import kotlin.math.floor
 
 @SuppressLint("DiscouragedApi")
-open class LandscapeBatteryM(private val context: Context, frameColor: Int) :
+open class LandscapeBatteryiOS16(private val context: Context, frameColor: Int) :
     BatteryDrawable() {
 
     // Need to load:
