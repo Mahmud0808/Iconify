8095d908b8e30c4a3204ad6504632598cb899562
Saurav Sonkar <122232433+sauravultra@users.noreply.github.com>
2025-04-05 17:40:53 +0530

    Update LandscapeBatteryiOS16.kt

--- app/src/main/java/com/drdisagree/iconify/xposed/modules/batterystyles/LandscapeBatteryiOS16.kt
+++ app/src/main/java/com/drdisagree/iconify/xposed/modules/batterystyles/LandscapeBatteryiOS16.kt
@@ -582,7 +582,7 @@ open class LandscapeBatteryiOS16(private val context: Context, frameColor: Int) 
     private fun updateSize() {
         val b = bounds
         if (b.isEmpty) {
-            scaleMatrix.setScale(1f, 1f)
+            scaleMatrix.setScale(3f, 3f)
         } else {
             scaleMatrix.setScale((b.right / WIDTH), (b.bottom / HEIGHT))
         }
@@ -641,8 +641,8 @@ open class LandscapeBatteryiOS16(private val context: Context, frameColor: Int) 
 
     companion object {
         private val TAG = LandscapeBatteryL::class.java.simpleName
-        private const val WIDTH = 26f
-        private const val HEIGHT = 15f
+        private const val WIDTH = 24f
+        private const val HEIGHT = 12f
         private const val CRITICAL_LEVEL = 15
 
         // On a 24x12 grid, how wide to make the fill protection stroke.
