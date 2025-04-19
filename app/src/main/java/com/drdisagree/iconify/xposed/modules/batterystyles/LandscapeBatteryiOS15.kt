1ad7985beb1e6d5b9e0ea81ff33752839cef6620
Saurav Sonkar <122232433+sauravultra@users.noreply.github.com>
2025-04-19 14:44:50 +0530

    Update LandscapeBatteryiOS15.kt

--- app/src/main/java/com/drdisagree/iconify/xposed/modules/batterystyles/LandscapeBatteryiOS15.kt
+++ app/src/main/java/com/drdisagree/iconify/xposed/modules/batterystyles/LandscapeBatteryiOS15.kt
@@ -172,8 +172,8 @@ open class LandscapeBatteryiOS15(private val context: Context, frameColor: Int) 
 
     init {
         val density = context.resources.displayMetrics.density
-        intrinsicHeight = (HEIGHT * density).toInt()
-        intrinsicWidth = (WIDTH * density).toInt()
+        intrinsicHeight = (HEIGHT * 1.25 * density).toInt()
+        intrinsicWidth = (WIDTH *0.85* density).toInt()
 
         val res = context.resources
         val levels = res.obtainTypedArray(
