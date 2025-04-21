b5d3d8c8352d3a3ddae06717169402372bccbc85
Saurav Sonkar <122232433+sauravultra@users.noreply.github.com>
2025-04-22 00:33:39 +0530

    Update SliderPreference.kt

--- app/src/main/java/com/drdisagree/iconify/ui/preferences/SliderPreference.kt
+++ app/src/main/java/com/drdisagree/iconify/ui/preferences/SliderPreference.kt
@@ -53,7 +53,7 @@ class SliderPreference(
 
     @Suppress("unused")
     private var sliderValue: TextView? = null
-    private var valueCount: Float
+    private var valueCount: Int
     private var valueFormat: String? = null
     private val outputScale: Float
     private val isDecimalFormat: Boolean
@@ -72,7 +72,7 @@ class SliderPreference(
 
         context.obtainStyledAttributes(attrs, R.styleable.SliderPreference).apply {
             updateConstantly = getBoolean(R.styleable.SliderPreference_updatesContinuously, false)
-            valueCount = getFloat(R.styleable.SliderPreference_valueCount, 0.5)
+            valueCount = getInt(R.styleable.SliderPreference_valueCount, 1)
             valueFrom = getFloat(R.styleable.SliderPreference_minVal, 0.0f)
             valueTo = getFloat(R.styleable.SliderPreference_maxVal, 100.0f)
             tickInterval = getFloat(R.styleable.SliderPreference_tickInterval, 0.5f)
