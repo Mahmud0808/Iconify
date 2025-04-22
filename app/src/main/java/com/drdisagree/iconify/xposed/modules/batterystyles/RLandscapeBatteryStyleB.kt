5044a8df9dc1e5d081e1df5f1282b1d431575b12
Saurav Sonkar <122232433+sauravultra@users.noreply.github.com>
2025-04-22 12:38:42 +0530

    Update RLandscapeBatteryStyleB.kt

--- app/src/main/java/com/drdisagree/iconify/xposed/modules/batterystyles/RLandscapeBatteryStyleB.kt
+++ app/src/main/java/com/drdisagree/iconify/xposed/modules/batterystyles/RLandscapeBatteryStyleB.kt
@@ -252,8 +252,8 @@ open class RLandscapeBatteryStyleB(private val context: Context, frameColor: Int
 
     init {
         val density = context.resources.displayMetrics.density
-        intrinsicHeight = (HEIGHT * 1.18f * density).toInt()
-        intrinsicWidth = (WIDTH * 0.85* density).toInt()
+        intrinsicHeight = (HEIGHT * 1.16f * density).toInt()
+        intrinsicWidth = (WIDTH * 0.86* density).toInt()
 
         val res = context.resources
         val levels = res.obtainTypedArray(
