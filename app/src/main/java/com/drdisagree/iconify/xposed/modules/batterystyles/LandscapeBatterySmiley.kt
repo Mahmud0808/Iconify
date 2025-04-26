0275127c741b95ce8a9708a9dd96f62ef2e9b556
Saurav Sonkar <122232433+sauravultra@users.noreply.github.com>
2025-04-26 19:02:42 +0530

    Update LandscapeBatterySmiley.kt

--- app/src/main/java/com/drdisagree/iconify/xposed/modules/batterystyles/LandscapeBatterySmiley.kt
+++ app/src/main/java/com/drdisagree/iconify/xposed/modules/batterystyles/LandscapeBatterySmiley.kt
@@ -434,7 +434,7 @@ open class LandscapeBatterySmiley(private val context: Context, frameColor: Int)
             val pctY = bounds.height() * 0.79f
 
             textPaint.color = fillColor
-            textQSPaint.color = fillColor
+            textQsPaint.color = fillColor
             if (isRotation) {
                 c.rotate(180f, pctX, pctY * 0.63f)
             }
