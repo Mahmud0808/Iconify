44f4d5dc0f894de4288549a8167c423e41991dde
Saurav Sonkar <122232433+sauravultra@users.noreply.github.com>
2025-06-11 13:11:30 +0530

    Update LandscapeBatteryL.kt

--- app/src/main/java/com/drdisagree/iconify/xposed/modules/batterystyles/LandscapeBatteryL.kt
+++ app/src/main/java/com/drdisagree/iconify/xposed/modules/batterystyles/LandscapeBatteryL.kt
@@ -640,7 +640,7 @@ open class LandscapeBatteryL(private val context: Context, frameColor: Int) :
         private val TAG = LandscapeBatteryL::class.java.simpleName
         private const val WIDTH = 24f
         private const val HEIGHT = 12f
-        private const val CRITICAL_LEVEL = 25
+        private const val CRITICAL_LEVEL = 30
 
         // On a 24x12 grid, how wide to make the fill protection stroke.
         // Scales when our size changes
