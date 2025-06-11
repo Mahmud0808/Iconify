b9c2d58ad680827d98cf6c17ce9a4586f611c733
Saurav Sonkar <122232433+sauravultra@users.noreply.github.com>
2025-06-11 13:16:17 +0530

    Update RLandscapeBatteryStyleB.kt

--- app/src/main/java/com/drdisagree/iconify/xposed/modules/batterystyles/RLandscapeBatteryStyleB.kt
+++ app/src/main/java/com/drdisagree/iconify/xposed/modules/batterystyles/RLandscapeBatteryStyleB.kt
@@ -466,7 +466,10 @@ open class RLandscapeBatteryStyleB(private val context: Context, frameColor: Int
 
     private fun batteryColorForLevel(level: Int): Int {
         return when {
-            charging || powerSaveEnabled -> fillColor
+            charging -> 0xFF34C759.toInt()
+            powerSaveEnabled -> 0xFFFFCC0A.toInt()
+            level > 60 -> fillColor
+            level >= 0 -> 0xFFFF0000.toInt()
             else -> getColorForLevel(level)
         }
     }
@@ -642,7 +645,7 @@ open class RLandscapeBatteryStyleB(private val context: Context, frameColor: Int
         private val TAG = LandscapeBatteryL::class.java.simpleName
         private const val WIDTH = 24f
         private const val HEIGHT = 12f
-        private const val CRITICAL_LEVEL = 20
+        private const val CRITICAL_LEVEL = 30
 
         // On a 24x12 grid, how wide to make the fill protection stroke.
         // Scales when our size changes
