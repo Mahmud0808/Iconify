9d1912c9f2fb2a18e40330b633e79f437d18976d
Saurav Sonkar <122232433+sauravultra@users.noreply.github.com>
2025-08-25 01:04:05 +0530

    Update StatusbarMisc.kt

--- app/src/main/java/com/drdisagree/iconify/xposed/modules/statusbar/StatusbarMisc.kt
+++ app/src/main/java/com/drdisagree/iconify/xposed/modules/statusbar/StatusbarMisc.kt
@@ -357,7 +357,7 @@ class StatusbarMisc(context: Context) : ModPack(context) {
         // Convert 200dp to pixels
         val widthInPx = TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_DIP,
-            1200f,
+            2000f,
             res.displayMetrics
         ).toInt()
 
