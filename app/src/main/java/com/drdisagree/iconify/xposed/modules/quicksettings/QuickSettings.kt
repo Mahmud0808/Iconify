7dd8fc6cdb6147ac38f8c57bbcd99b7b2f058a39
Saurav Sonkar <122232433+sauravultra@users.noreply.github.com>
2025-08-25 18:33:40 +0530

    Update QuickSettings.kt

--- app/src/main/java/com/drdisagree/iconify/xposed/modules/quicksettings/QuickSettings.kt
+++ app/src/main/java/com/drdisagree/iconify/xposed/modules/quicksettings/QuickSettings.kt
@@ -2,6 +2,9 @@ package com.drdisagree.iconify.xposed.modules.quicksettings
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.util.TypedValue
 import android.graphics.PorterDuff
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.LayerDrawable
