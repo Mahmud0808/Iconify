8df1d664e462a846277922912c40d05f2b2eebb7
Saurav Sonkar <122232433+sauravultra@users.noreply.github.com>
2025-08-25 11:36:46 +0530

    Update StatusbarMisc.kt

--- app/src/main/java/com/drdisagree/iconify/xposed/modules/statusbar/StatusbarMisc.kt
+++ app/src/main/java/com/drdisagree/iconify/xposed/modules/statusbar/StatusbarMisc.kt
@@ -349,23 +349,7 @@ class StatusbarMisc(context: Context) : ModPack(context) {
 
                 phoneStatusBarViewParam.moveStatusBarClock()
                 phoneStatusBarViewParam.background = ColorDrawable(Color.parseColor("#33000000"))
-            // Find the notification_icon_area view
-        val res = phoneStatusBarViewParam.resources
-        val iconAreaId = res.getIdentifier("notification_icon_area", "id", "com.android.systemui")
-        val iconArea = phoneStatusBarViewParam.findViewById<ViewGroup>(iconAreaId)
-
-        // Convert 200dp to pixels
-        val widthInPx = TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP,
-            4000f,
-            res.displayMetrics
-        ).toInt()
-
-        // Update layout width to 200dp
-        iconArea?.layoutParams?.let { lp ->
-            lp.width = widthInPx
-            iconArea.layoutParams = lp
-            }
+      
         
                  
 
