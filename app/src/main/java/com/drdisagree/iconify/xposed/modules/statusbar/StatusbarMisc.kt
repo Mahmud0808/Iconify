d280233faa2e08cc82caf25f466c36391b0b224a
Saurav Sonkar <122232433+sauravultra@users.noreply.github.com>
2025-08-25 20:02:01 +0530

    Update StatusbarMisc.kt

--- app/src/main/java/com/drdisagree/iconify/xposed/modules/statusbar/StatusbarMisc.kt
+++ app/src/main/java/com/drdisagree/iconify/xposed/modules/statusbar/StatusbarMisc.kt
@@ -349,7 +349,7 @@ class StatusbarMisc(context: Context) : ModPack(context) {
 
                 phoneStatusBarViewParam.moveStatusBarClock()
                 phoneStatusBarViewParam.background = ColorDrawable(Color.parseColor("#33000000"))
-
+            }
         // 3) Find status_bar_start_side_except_heads_up and set width to wrap_content
         val startSideId = res.getIdentifier(
             "status_bar_start_side_except_heads_up",
